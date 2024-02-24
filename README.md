e620ffa70d6cc44162b80ed8ece28487f590bea9
preciousken <pkehinde27@gmail.com>
2024-02-24 13:51:49 +0100

    created a public module

---
+++ README.md
@@ -0,0 +1,37 @@
+# Rizeology snapit
+
+## Installation
+
+Make sure you have Node.js and npm installed. Install the required dependencies using the following command:
+
+```bash
+npm install rizeology-snapit
+```
+
+## Usage
+
+```js
+const {Snapit} = require("rizeology-snapit");
+const snapit = new Snapit();
+
+let delay = 6*1000 // 6seconds delay for website to fully load
+
+// take a screenshot and add delay before taking a screenshot
+snapit.snapAfter("https://website-url-or-image-url.com",delay)
+
+// generate a pdf file
+snapit.genPDFAfter("https://website-url-or-image-url.com",delay)
+
+```
+
+
+## Contributing
+Feel free to contribute by opening issues or submitting pull requests. Contributions are welcome!
+
+## License
+This project is licensed under the MIT License.
+
+#### made with 💘 by preciousken
+
+
+
---
+++ snapit.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+class Snapit {
+    constructor() {
+    }
+
+
+    /**
+     * @param {string} url - The url of the website to screenshot
+     * @param {number} seconds - The number of seconds to wait before taking the screenshot
+     */
+    snapAfter(url, seconds = 6000) {
+        return new Promise((resolve, reject) => {
+            const waitFor = async (ms) => {
+                return new Promise(resolve => {
+                    setTimeout(resolve, ms);
+                });
+            }
+            let path = null;
+            
+            (async () => {
+                try {
+                    const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+    
+                page.setDefaultNavigationTimeout(0);
+                await page.goto(url);
+    
+    
+                await this.confirmDir();
+    
+                path = 'uploads/' + this.generateUniqueName() + '.png';
+    
+                await waitFor(seconds);
+                await page.screenshot({path: path,fullPage: true});
+                
+                await browser.close();
+                resolve(path);
+                } catch (error) {
+                reject(error)
+                }
+            })();
+        })
+    }
+
+    generateUniqueName() {
+        const date = new Date();
+        const uniqueName = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+        return uniqueName;
+    }
+
+    // this code class method snippet checks if the uploads directory exists, if not, it creates it.
+    confirmDir() {
+        const dir = './uploads';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+    }
+
+
+    /**
+     * @param {string} url - The url of the website to generate pdf
+     * @param {number} seconds - The number of seconds to wait before generating the pdf
+     */
+    genPDFAfter(url, seconds = 6000) {
+        return new Promise((resolve, reject) => {
+            const waitFor = async (ms) => {
+                return new Promise(resolve => {
+                    setTimeout(resolve, ms);
+                });
+            }
+            let path = null;
+            
+            (async () => {
+               try {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+    
+                page.setDefaultNavigationTimeout(0);
+                await page.goto(url);
+    
+                await waitFor(seconds);
+    
+                await this.confirmDir();
+    
+                path = 'uploads/' + this.generateUniqueName() + '.pdf';
+    
+                // this code snippet generate string name from current date and time
+    
+                await page.pdf({
+                    path: path,
+                    format: 'letter',
+                  });
+                await browser.close();
+                resolve(path);
+               } catch (error) {
+                reject(error)
+               }
+            })();
+        })
+
+    }
+}
+
+module.exports = {
+    Snapit
+}
